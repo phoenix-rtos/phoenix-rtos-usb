08f9962eaec2ec39f5fb6d20baf4f68bbeebfa91
Adam Greloch <adam.greloch@phoenix-rtos.com>
2026-02-27 15:53:14 +0100

    !libusb/procdriver: accept prio and nthreads as usb_driverProcRun args
    Also, the usb_driverProcRun now explicitly exits on failure.
    
    JIRA: RTOS-1214
    (cherry picked from commit 3ffbe3cc72e104a7e5b1db595b0fe0d08ad3f792)

--- libusb/include/usbprocdriver.h
+++ libusb/include/usbprocdriver.h
@@ -18,7 +18,12 @@
 #include <usbdriver.h>
 
 
-int usb_driverProcRun(usb_driver_t *driver, void *args);
+/*
+ * Initializes the given driver and spawns a thread pool of nthreads of prio priority to handle usbhost events.
+ * Threads concurrently process insertion/deletion/completion events with provided driver->handlers. Calls exit(1)
+ * on any failure.
+ */
+__attribute__((noreturn)) void usb_driverProcRun(usb_driver_t *driver, unsigned int prio, unsigned int nthreads, void *args);
 
 
 #endif
